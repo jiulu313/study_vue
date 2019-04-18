eb39a0c7fa131ccc840a42b5ff53db40ff6e52e4
zhanghongjun <zhanghongjun01@meicai.cn>
2019-04-18 15:17:47 +0800

    添加基本的指令

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     {{msg}}
+
+    <p v-for="(book ,index) in books">{{index}} : {{book}}</p>
+
+
+    <div style="background-color:#ffaa22;">
+
+      <p v-if="show" v-on:click="onWhoAreYou">你是谁</p>
+
+
+      <button v-on:click="onClickMe">点我</button>
+
+
+    </div>
+
   </div>
 </template>
 
@@ -9,7 +23,23 @@
     name: 'HelloWorld',
     data() {
       return {
-        msg: 'hello,world'
+        msg: 'hello,world',
+
+        books:['30天精通Java','LAMP网站环境搭建','JS从入门到放弃'],
+
+        show:true,
+
+      }
+    },
+
+
+    methods:{
+      onWhoAreYou(){
+        alert("我是张三");
+      },
+
+      onClickMe(){
+        this.show = !this.show;
       }
     }
   }
